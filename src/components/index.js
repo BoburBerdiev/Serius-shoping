aa45cdc6e6d15395090d8ad683d4c1c70e3cf7e8
BoburBerdiev <sirofim012@gmail.com>
2024-01-05 16:58:51 +0500

    Bismillah

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,4 @@
 
 export { default as Navbar } from "./navbar/navbar";
 export { default as ImageUI } from "./ImageUI/Image-UI";
-export { default as IndexBanner } from "./index-banner/index-banner";
-export { default as IndexCard } from "./index-card/index-card";
 export { default as Footer } from "./footer/footer";
-export { default as SectionTitle } from "./section-title/section-title";
-export { default as Video } from "./video/video";
-export { default as SwiperSection } from "./swiper-section/swiper-section";
-export { default as  ConfigurationsSection} from "./configuration-section/configurations-section";
-export { default as  PositionCard} from "./position-card/position-card";
