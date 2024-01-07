f595e820c7932de608cd19ad14ff3a9df129c0f4
zokirjonovshohruh <shohruhfrontend@gmail.com>
2024-01-08 01:12:39 +0500

    ++

--- src/components/CategoryCardUI/CategoryCard-UI.jsx
+++ src/components/CategoryCardUI/CategoryCard-UI.jsx
@@ -4,10 +4,10 @@ import { LuArrowUpRight } from "react-icons/lu";
 
 const CategoryCardUI = ({link,src, alt, text, textPosition, iconPosition, grid  }) => {
   return (
-    <a href={link} className={`w-full relative block rounded-lg overflow-hidden group h-[240px] ${grid}`}>
+    <a href={link ? link : '#'} className={`w-full relative block rounded-lg overflow-hidden group h-[240px] ${grid}`}>
       <ImageUI imgStyle={'object-cover group-hover:scale-125'} src={src} alt={alt}/>
-      <h2 className={`text-white font-rubik text-xl font-medium absolute top-[12%] left-[7%] ${textPosition} `}>{text}</h2>
-      <span className={`absolute p-2 rounded-full bg-[#D9D9D9] bottom-[7%] right-[7%] ${iconPosition}`}><LuArrowUpRight className='w-6 h-6 lg:h-7 lg:w-7 text-white' /></span>
+      <h2 className={`text-white font-rubik text-xl font-medium absolute top-[12%] left-[7%] sm:top-5 sm:left-5 ${textPosition} `}>{text}</h2>
+      <span className={`absolute p-2 rounded-full bg-[#D9D9D9] bottom-[7%] right-[7%] sm:bottom-5 sm:right-5 ${iconPosition ? iconPosition : 'bottom-[30px] right-[30px]'}`}><LuArrowUpRight className='w-6 h-6 lg:h-7 lg:w-7 text-white' /></span>
     </a>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,27 +4,29 @@ import { CardUI, CategoryCardUI } from "@/components";
 export default function Home() {
   return (
     <>
-    <div className="container py-20">
-      <div className="grid md:grid-cols-3 lg:grid-cols-4 grid-cols-1 gap-5 md:gap-8 py-20">
-        <CardUI src={'/Card.png'} rows={true} title={'Чехол для iPhone 11 Pro Simply TPU'} price={'5 000 000 сум'} percent={'-25%'} salePrice={'85 000 сум'}/>
-        <CardUI src={'/Card.png'} rows={true} title={'Чехол для iPhone 11 Pro Simply TPU'} price={'65 000 сум'} percent={'-25%'} salePrice={'85 000 сум'}/>
-        <CardUI src={'/Card.png'} rows={true} title={'Чехол для iPhone 11 Pro Simply TPU '} price={'65 000 сум'} percent={'-25%'} salePrice={'85 000 сум'}/>
-      </div>
-    </div>
-    <section className="py-20">
+    <section className="py-20 md:pt-40">
       <div className="container">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-        <CategoryCardUI grid={'md:row-span-2 md:h-full'} text={'Чехлы'} link={'#'} textPosition={'md:top-10 md:left-10'} iconPosition={''} src={'/mobile-images/categories/phone-case.png'}/>
-        <CategoryCardUI src={'/mobile-images/categories/phone-case.png'}/>
-        <CategoryCardUI src={'/mobile-images/categories/phone-case.png'}/>
-        <CategoryCardUI src={'/mobile-images/categories/phone-case.png'}/>
-        <CategoryCardUI src={'/mobile-images/categories/phone-case.png'}/>
-        <CategoryCardUI src={'/mobile-images/categories/phone-case.png'}/>
-        <CategoryCardUI src={'/mobile-images/categories/phone-case.png'}/>
-
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-7 gap-8">
+        <CategoryCardUI grid={'md:row-span-2 md:h-full lg:col-span-3'} text={'Чехлы'} link={'#'} textPosition={'lg:top-10 lg:left-10'} iconPosition={'lg:bottom-auto lg:top-10 lg:right-10'} src={'/mobile-images/categories/phone-case.png'}/>
+        <CategoryCardUI grid={'lg:col-span-2'} text={'Стекла и пленки'} textPosition={'lg:top-[30px] left-7'} src={'/mobile-images/categories/phone-glass.png'}/>
+        <CategoryCardUI grid={'sm:col-span-2 md:col-span-1 lg:col-span-2'} textPosition={'lg:top-[30px] left-7'} text={'Гаджеты'} src={'/mobile-images/categories/gadgets.png'}/>
+        <CategoryCardUI grid={'lg:col-span-4'} text={'Аудио'} iconPosition={'lg:top-7 lg:right-11 lg:bottom-auto'} textPosition={'lg:top-[30px] left-7'} src={'/mobile-images/categories/audio.png'}/>
+        <CategoryCardUI grid={'lg:col-span-3'} iconPosition={'lg:top-[30px] lg:bottom-auto lg:right-[30px]'} textPosition={'lg:bottom-8 lg:left-6 lg:top-auto'} text={'Аксессуары для смарт часов'} src={'/mobile-images/categories/smart-watch.png'}/>
+        <CategoryCardUI grid={'lg:col-span-2'} text={'Кабели и ЗУ'} textPosition={'lg:top-[30px] left-7'} src={'/mobile-images/categories/cables.png'}/>
+        <CategoryCardUI grid={'lg:col-span-2'} text={'Разное'} textPosition={'lg:top-[30px] left-7'} src={'/mobile-images/categories/other-products.png'}/>
         </div>
       </div>
     </section>
+
+    <div className="container py-20">
+      <div className="grid md:grid-cols-3 lg:grid-cols-4 grid-cols-1 gap-5 md:gap-8 py-20">
+        <CardUI src={'/Card.png'} title={'Чехол для iPhone 11 Pro Simply TPU'} price={'5 000 000 сум'} percent={'-25%'} salePrice={'85 000 сум'}/>
+        <CardUI src={'/Card.png'} title={'Чехол для iPhone 11 Pro Simply TPU'} price={'65 000 сум'} percent={'-25%'} salePrice={'85 000 сум'}/>
+        <CardUI src={'/Card.png'} title={'Чехол для iPhone 11 Pro Simply TPU '} price={'65 000 сум'} percent={'-25%'} salePrice={'85 000 сум'}/>
+        <CardUI src={'/Card.png'} title={'Чехол для iPhone 11 Pro Simply TPU '} price={'65 000 сум'}  salePrice={'85 000 сум'}/>
+      </div>
+    </div>
+    
     
     </>
   )
