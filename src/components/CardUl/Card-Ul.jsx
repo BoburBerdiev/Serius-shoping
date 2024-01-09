fafac1d57cee8a5a6ac83ab5929a545d79655cf3
BoburBerdiev <sirofim012@gmail.com>
2024-01-09 17:37:18 +0500

    tekwiriw ucun

--- src/components/CardUl/Card-Ul.jsx
+++ src/components/CardUl/Card-Ul.jsx
@@ -25,37 +25,39 @@ const CardUI = ({image, alt, title, price, salePrice, percent, rows}) => {
   return (
     <div className={`flex ${rows ? 'items-center' : 'flex-col  h-full'}  gap-3.5 md:gap-6`}>
       <div className={`${rows ? 'w-[40%]' : 'w-full'}`}>
-        <div className={`relative w-full ${rows ? 'h-[180px] md:h-[240px] lg:h-[260px] ' : 'h-[220px] md:h-[270px] lg:h-[310px]'} overflow-hidden rounded-lg`}>
-          <div className="w-full h-full ">
-            <Swiper
-              id={"mySwiper1"}
-              slidesPerView={1}
-              autoplay={{
-                delay: 2800,
-                disableOnInteraction: false,
-              }}
-              loop={"true"}
-              pagination={{ clickable: true }}
-              navigation={false}
-              modules={[Autoplay ,Pagination]}
-              className="w-full  mySwiper h-full"
-            >
-              {productImage?.map((product) => (
-                  <SwiperSlide key={product.id} >
-                    <div className={"relative w-full h-full swiper1"}>
-                      <ImageUI src={product.image} alt={product.alt} imgStyle={'object-cover'}/>  
-                    </div>
-                  </SwiperSlide>
-                ))}
-            </Swiper>
+
+                  <Swiper
+                      id={"mySwiper1"}
+                      slidesPerView={1}
+                      autoplay={{
+                          delay: 2800,
+                          disableOnInteraction: false,
+                      }}
+                      loop={true}
+                      pagination={{clickable: true}}
+                      navigation={false}
+                      modules={[Autoplay, Pagination]}
+                      className={` mySwiper w-full `}
+                  >
+
+                      {productImage?.map((product) => (
+                          <SwiperSlide key={product.id}  >
+
+                              <div className={`w-full h-full relative ${rows ? 'h-[180px] md:h-[240px] lg:h-[260px] ' : 'h-[220px] md:h-[270px] lg:h-[310px]'} overflow-hidden rounded-lg`}>
+                                  <ImageUI src={product.image} imgStyle={'object-cover'}/>
+
+                              </div>
+                          </SwiperSlide>
+                          ))}
+                  </Swiper>
+
+              {
+                  percent && <span
+                      className="py-1 px-2 bg-currentRed absolute text-white text-xs md:text-sm font-rubik font-medium rounded-lg top-3 right-3 z-10  ">Sale</span>
+              }
           </div>
-          {
-           percent && <span className="py-1 px-2 bg-currentRed absolute text-white text-xs md:text-sm font-rubik font-medium rounded-lg top-3 right-3 z-10  ">Sale</span>
-          }
-        </div>
-      </div>
-      <div className={`gap-y-4 flex  flex-col h-full ${rows ? 'w-1/2 justify-center' : 'w-full justify-between'}`}>
-      <div className="space-y-3 lg:space-y-[18px]">
+          <div className={`gap-y-4 flex  flex-col h-full ${rows ? 'w-1/2 justify-center' : 'w-full justify-between'}`}>
+              <div className="space-y-3 lg:space-y-[18px]">
         <h5 className="font-rubik text-sm  md:text-lg break-all">
           { title }
         </h5>
