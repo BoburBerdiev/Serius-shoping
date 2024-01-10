d811ebc176b9fbbd07b81d1bc51b2fa2b4519851
Umar-02 <umarfront@gmail.com>
2024-01-10 15:56:49 +0500

    filter

--- src/components/SectionTitleUI/SectionTitle-UI.jsx
+++ src/components/SectionTitleUI/SectionTitle-UI.jsx
@@ -3,12 +3,12 @@ import { ButtonUI } from '..'
 
 const SectionTitleUI = ({isBorder, title, titleNum, href}) => {
   return (
-    <div className={`flex font-rubik items-end text-darkBlue justify-between border-borderGrey mb-4 md:mb-[30px] ${isBorder ? '' : 'pb-1.5 md:pb-2.5 border-b '}`} >
-      <div className='flex items-end gap-2.5 '>
+    <div className={`flex font-rubik items-end text-darkBlue justify-between border-borderGrey mb-4 md:mb-[30px] leading-normal ${isBorder ? '' : 'pb-1.5 md:pb-2.5 border-b '}`} >
+      <div className='flex items-center gap-2.5 '>
         <h2 className=' font-medium text-2xl md:text-3xl lg:text-4xl'>{title}</h2>
         {
           titleNum && 
-          <span className='text-currentGrey text-sm '>{titleNum}</span>
+          <span className='text-currentGrey text-sm md:text-xl lg:text-[26px] '>{titleNum}</span>
         }
       </div>
       {
--- src/pages/product/index.js
+++ src/pages/product/index.js
@@ -1,9 +1,42 @@
-
+import { BreadcrumbUI, CardUI, Navbar, SectionTitleUI, SectionUI } from "@/components"
+const productImage = [
+  {
+    id: 1,
+    image : '/Card.png',
+    alt: ''
+  },
+  {
+    id: 2,
+    image : '/Card.png',
+    alt: ''
+  },
+  {
+    id: 3,
+    image : '/Card.png',
+    alt: ''
+  },
+]
 const index = () => {
   return (
-    <div>
+    <>
+    <SectionUI customPadding={'pt-[140px] md:pt-40 pb-10 font-rubik pruduct-inner relative '}>
+      <div className="space-y-5 md:space-y-[30px]">
+        <BreadcrumbUI/>
+        <SectionTitleUI title={'Чехлы для телефонов'} titleNum={'(35)'} />
+        <div className="grid grid-cols-5">
+          <div>
 
-    </div>
+          </div>
+          <div className="col-span-4 grid grid-cols-2  md:grid-cols-3 lg:grid-cols-4 gap-5">
+          <CardUI imageArr={productImage} src={'/Card.png'} title={'Чехол для iPhone 11 Pro Simply TPU'} price={'5 000 000 сум'} salePrice={'85 000 сум'}/>
+          <CardUI imageArr={productImage} src={'/Card.png'} title={'Чехол для iPhone 11 Pro Simply TPU'} price={'5 000 000 сум'} salePrice={'85 000 сум'}/>
+          <CardUI imageArr={productImage} src={'/Card.png'} title={'Чехол для iPhone 11 Pro Simply TPU'} price={'5 000 000 сум'} salePrice={'85 000 сум'}/>
+          <CardUI imageArr={productImage} src={'/Card.png'} title={'Чехол для iPhone 11 Pro Simply TPU'} price={'5 000 000 сум'} salePrice={'85 000 сум'}/>
+          </div>
+        </div>
+      </div>
+    </SectionUI>
+    </>
   )
 }
 
