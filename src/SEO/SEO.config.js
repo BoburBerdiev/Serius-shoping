90f86d52017c0ed394aa8daa82c06601e4c2b375
Umar-02 <umarfront@gmail.com>
2024-01-26 01:43:43 +0500

    SEO update

--- src/SEO/SEO.config.js
+++ src/SEO/SEO.config.js
@@ -23,54 +23,54 @@ export const seoContent = {
             ogDescription: 'Погрузитесь в наш каталог и купите широкий ассортимент недорогих мобильных аксессуаров в Sirius-tech. От изящных чехлов до инновационных гаджетов — найдите идеальный аксессуар, не жертвуя ценой и стилем.',
         },
     },
-    creditSEO: {
-        uz: {
-            title: "Byudjetingiz uchun moslashuvchan to'lov imkoniyatlari | Sirius-tech mobil aksessuarlari",
-            description: "Sirius-tech-dagi moslashuvchan to'lov turlarimiz bilan muammosiz xarid qilishdan rohatlaning. Hamyonbop mobil aksessuarlar bir marta bosish masofasida. Ehtiyojlaringizga moslashtirilgan qulay va xavfsiz toʻlov usullarini kashf eting.",
-            ogDescription: "Sirius-tech-dagi moslashuvchan to'lov turlarimiz bilan muammosiz xarid qilishdan rohatlaning. Hamyonbop mobil aksessuarlar bir marta bosish masofasida. Ehtiyojlaringizga moslashtirilgan qulay va xavfsiz toʻlov usullarini kashf eting.",
-        },
-        ru: {
-            title: 'Гибкие варианты оплаты для вашего бюджета | Мобильные аксессуары Sirius-tech',
-            description: 'Наслаждайтесь беспроблемными покупками с нашими гибкими вариантами оплаты в Sirius-tech. Доступные мобильные аксессуары находятся на расстоянии одного клика. Откройте для себя удобные и безопасные способы оплаты, соответствующие вашим потребностям.',
-            ogDescription: 'Наслаждайтесь беспроблемными покупками с нашими гибкими вариантами оплаты в Sirius-tech. Доступные мобильные аксессуары находятся на расстоянии одного клика. Откройте для себя удобные и безопасные способы оплаты, соответствующие вашим потребностям.',
-        },
-    },
-    insuranceSEO: {
-        uz: {
-            title: "Xaridingizni Sirius-tech mobil aksessuarlar sug'urtasi bilan himoya qiling",
-            description: "Sirius-tech da mobil aksessuarlarni sug'urtalash imkoniyatlari bilan tanishing. Qurilmalaringiz va aksessuarlaringizni himoya qiling. Har bir xaridda xotirjamlik uchun qulay qamrov.",
-            ogDescription: "Sirius-tech da mobil aksessuarlarni sug'urtalash imkoniyatlari bilan tanishing. Qurilmalaringiz va aksessuarlaringizni himoya qiling. Har bir xaridda xotirjamlik uchun qulay qamrov.",
-        },
-        ru: {
-            title: 'Защитите свою покупку с помощью страхования мобильных аксессуаров Sirius-tech',
-            description: 'Изучите наши варианты страхования мобильных аксессуаров в Sirius-tech. Обеспечьте защиту своих устройств и аксессуаров. Доступное покрытие для душевного спокойствия при каждой покупке.',
-            ogDescription: 'Изучите наши варианты страхования мобильных аксессуаров в Sirius-tech. Обеспечьте защиту своих устройств и аксессуаров. Доступное покрытие для душевного спокойствия при каждой покупке.',
-        },
-    },
-    serviceSEO: {
-        uz: {
-            title: 'Ajoyib xizmatlar, hamyonbop aksessuarlar | Sirius texnologiyasi',
-            description: "Sirius-tech da ajoyib xizmat ko'rsatishga sodiqligimizni bilib oling. Tez yetkazib berishdan do'stona mijozlarni qo'llab-quvvatlashgacha, biz mobil aksessuarlarni xarid qilish tajribangizni muammosiz va yoqimli qilish uchun qo'shimcha masofani bosib o'tamiz.",
-            ogDescription: "Sirius-tech da ajoyib xizmat ko'rsatishga sodiqligimizni bilib oling. Tez yetkazib berishdan do'stona mijozlarni qo'llab-quvvatlashgacha, biz mobil aksessuarlarni xarid qilish tajribangizni muammosiz va yoqimli qilish uchun qo'shimcha masofani bosib o'tamiz.",
-        },
-        ru: {
-            title: 'Исключительный сервис, доступные аксессуары | Sirius-tech',
-            description: 'Откройте для себя нашу приверженность исключительному обслуживанию в Sirius-tech. От быстрой доставки до дружелюбной поддержки клиентов — мы делаем все возможное, чтобы сделать покупку мобильных аксессуаров комфортной и приятной.',
-            ogDescription: 'Откройте для себя нашу приверженность исключительному обслуживанию в Sirius-tech. От быстрой доставки до дружелюбной поддержки клиентов — мы делаем все возможное, чтобы сделать покупку мобильных аксессуаров комфортной и приятной.',
-        },
-    },
-    aboutUsSEO: {
-        uz: {
-            title: 'Sirius-tech ning ochilishi: hamyonbop mobil aksessuarlar, bizning hikoyamiz, sizning uslubingiz',
-            description: "Sirius-tech ortidagi voqea haqida bilib oling. Biz arzon va zamonaviy mobil aksessuarlarni taqdim etishga ishtiyoqimiz bor. Eng soʻnggi texnologiya yoʻnalishlarini hamma uchun ochiq qilish missiyamizga qoʻshiling.",
-            ogDescription: "Sirius-tech ortidagi voqea haqida bilib oling. Biz arzon va zamonaviy mobil aksessuarlarni taqdim etishga ishtiyoqimiz bor. Eng soʻnggi texnologiya yoʻnalishlarini hamma uchun ochiq qilish missiyamizga qoʻshiling.",
-        },
-        ru: {
-            title: 'Представляем Sirius-tech: доступные мобильные аксессуары, наша история, ваш стиль',
-            description: 'Узнайте об истории Sirius-tech. Мы стремимся предоставлять доступные и стильные мобильные аксессуары. Присоединяйтесь к нашей миссии — сделать новейшие технологические тенденции доступными для каждого.',
-            ogDescription: 'Узнайте об истории Sirius-tech. Мы стремимся предоставлять доступные и стильные мобильные аксессуары. Присоединяйтесь к нашей миссии — сделать новейшие технологические тенденции доступными для каждого.',
-        },
-    },
+    // creditSEO: {
+    //     uz: {
+    //         title: "Byudjetingiz uchun moslashuvchan to'lov imkoniyatlari | Sirius-tech mobil aksessuarlari",
+    //         description: "Sirius-tech-dagi moslashuvchan to'lov turlarimiz bilan muammosiz xarid qilishdan rohatlaning. Hamyonbop mobil aksessuarlar bir marta bosish masofasida. Ehtiyojlaringizga moslashtirilgan qulay va xavfsiz toʻlov usullarini kashf eting.",
+    //         ogDescription: "Sirius-tech-dagi moslashuvchan to'lov turlarimiz bilan muammosiz xarid qilishdan rohatlaning. Hamyonbop mobil aksessuarlar bir marta bosish masofasida. Ehtiyojlaringizga moslashtirilgan qulay va xavfsiz toʻlov usullarini kashf eting.",
+    //     },
+    //     ru: {
+    //         title: 'Гибкие варианты оплаты для вашего бюджета | Мобильные аксессуары Sirius-tech',
+    //         description: 'Наслаждайтесь беспроблемными покупками с нашими гибкими вариантами оплаты в Sirius-tech. Доступные мобильные аксессуары находятся на расстоянии одного клика. Откройте для себя удобные и безопасные способы оплаты, соответствующие вашим потребностям.',
+    //         ogDescription: 'Наслаждайтесь беспроблемными покупками с нашими гибкими вариантами оплаты в Sirius-tech. Доступные мобильные аксессуары находятся на расстоянии одного клика. Откройте для себя удобные и безопасные способы оплаты, соответствующие вашим потребностям.',
+    //     },
+    // },
+    // insuranceSEO: {
+    //     uz: {
+    //         title: "Xaridingizni Sirius-tech mobil aksessuarlar sug'urtasi bilan himoya qiling",
+    //         description: "Sirius-tech da mobil aksessuarlarni sug'urtalash imkoniyatlari bilan tanishing. Qurilmalaringiz va aksessuarlaringizni himoya qiling. Har bir xaridda xotirjamlik uchun qulay qamrov.",
+    //         ogDescription: "Sirius-tech da mobil aksessuarlarni sug'urtalash imkoniyatlari bilan tanishing. Qurilmalaringiz va aksessuarlaringizni himoya qiling. Har bir xaridda xotirjamlik uchun qulay qamrov.",
+    //     },
+    //     ru: {
+    //         title: 'Защитите свою покупку с помощью страхования мобильных аксессуаров Sirius-tech',
+    //         description: 'Изучите наши варианты страхования мобильных аксессуаров в Sirius-tech. Обеспечьте защиту своих устройств и аксессуаров. Доступное покрытие для душевного спокойствия при каждой покупке.',
+    //         ogDescription: 'Изучите наши варианты страхования мобильных аксессуаров в Sirius-tech. Обеспечьте защиту своих устройств и аксессуаров. Доступное покрытие для душевного спокойствия при каждой покупке.',
+    //     },
+    // },
+    // serviceSEO: {
+    //     uz: {
+    //         title: 'Ajoyib xizmatlar, hamyonbop aksessuarlar | Sirius texnologiyasi',
+    //         description: "Sirius-tech da ajoyib xizmat ko'rsatishga sodiqligimizni bilib oling. Tez yetkazib berishdan do'stona mijozlarni qo'llab-quvvatlashgacha, biz mobil aksessuarlarni xarid qilish tajribangizni muammosiz va yoqimli qilish uchun qo'shimcha masofani bosib o'tamiz.",
+    //         ogDescription: "Sirius-tech da ajoyib xizmat ko'rsatishga sodiqligimizni bilib oling. Tez yetkazib berishdan do'stona mijozlarni qo'llab-quvvatlashgacha, biz mobil aksessuarlarni xarid qilish tajribangizni muammosiz va yoqimli qilish uchun qo'shimcha masofani bosib o'tamiz.",
+    //     },
+    //     ru: {
+    //         title: 'Исключительный сервис, доступные аксессуары | Sirius-tech',
+    //         description: 'Откройте для себя нашу приверженность исключительному обслуживанию в Sirius-tech. От быстрой доставки до дружелюбной поддержки клиентов — мы делаем все возможное, чтобы сделать покупку мобильных аксессуаров комфортной и приятной.',
+    //         ogDescription: 'Откройте для себя нашу приверженность исключительному обслуживанию в Sirius-tech. От быстрой доставки до дружелюбной поддержки клиентов — мы делаем все возможное, чтобы сделать покупку мобильных аксессуаров комфортной и приятной.',
+    //     },
+    // },
+    // aboutUsSEO: {
+    //     uz: {
+    //         title: 'Sirius-tech ning ochilishi: hamyonbop mobil aksessuarlar, bizning hikoyamiz, sizning uslubingiz',
+    //         description: "Sirius-tech ortidagi voqea haqida bilib oling. Biz arzon va zamonaviy mobil aksessuarlarni taqdim etishga ishtiyoqimiz bor. Eng soʻnggi texnologiya yoʻnalishlarini hamma uchun ochiq qilish missiyamizga qoʻshiling.",
+    //         ogDescription: "Sirius-tech ortidagi voqea haqida bilib oling. Biz arzon va zamonaviy mobil aksessuarlarni taqdim etishga ishtiyoqimiz bor. Eng soʻnggi texnologiya yoʻnalishlarini hamma uchun ochiq qilish missiyamizga qoʻshiling.",
+    //     },
+    //     ru: {
+    //         title: 'Представляем Sirius-tech: доступные мобильные аксессуары, наша история, ваш стиль',
+    //         description: 'Узнайте об истории Sirius-tech. Мы стремимся предоставлять доступные и стильные мобильные аксессуары. Присоединяйтесь к нашей миссии — сделать новейшие технологические тенденции доступными для каждого.',
+    //         ogDescription: 'Узнайте об истории Sirius-tech. Мы стремимся предоставлять доступные и стильные мобильные аксессуары. Присоединяйтесь к нашей миссии — сделать новейшие технологические тенденции доступными для каждого.',
+    //     },
+    // },
     contactSEO: {
         uz: {
             title: "Sirius-tech bilan bog'laning - arzon mobil aksessuarlar",
@@ -83,30 +83,18 @@ export const seoContent = {
             ogDescription: 'Есть вопросы или нужна помощь? Свяжитесь с Sirius-tech для получения оперативной и дружелюбной поддержки. Мы здесь, чтобы помочь вам найти идеальные мобильные аксессуары по доступной цене, отвечающие вашим потребностям.',
         },
     },
-    newsSEO: {
-        uz: {
-            title: "Sirius-tech kompaniyasining so'nggi yangiliklari, aksiyalar va mobil aksessuarlardagi tendentsiyalaridan xabardor bo'ling",
-            description: "Mobil aksessuarlar tendentsiyalari haqidagi eng yangi yangilanishlar, eksklyuziv yangiliklar uchun bizning blogimizga kiring va qiziqarli sotuvlarimizni o'tkazib yubormang. Sirius-tech’da arzon texnologik yechimlar va uslub ilhomini kashf eting.",
-            ogDescription: "Mobil aksessuarlar tendentsiyalari haqidagi eng yangi yangilanishlar, eksklyuziv yangiliklar uchun bizning blogimizga kiring va qiziqarli sotuvlarimizni o'tkazib yubormang. Sirius-tech’da arzon texnologik yechimlar va uslub ilhomini kashf eting.",
-        },
-        ru: {
-            title: 'Будьте в курсе последних новостей и тенденций Sirius-tech в области мобильных аксессуаров.',
-            description: 'Загляните в наш блог, чтобы узнать самые свежие новости о тенденциях в области мобильных аксессуаров, эксклюзивные новости и не пропустите наши захватывающие распродажи. Откройте для себя доступные технологические решения и вдохновение для стиля в Sirius-tech.',
-            ogDescription: 'Загляните в наш блог, чтобы узнать самые свежие новости о тенденциях в области мобильных аксессуаров, эксклюзивные новости и не пропустите наши захватывающие распродажи. Откройте для себя доступные технологические решения и вдохновение для стиля в Sirius-tech.',
-        },
-    },
-    driveSEO: {
-        uz: {
-            title: '- Test-drive ',
-            description: "",
-            ogDescription: "",
-        },
-        ru: {
-            title: '- тест-драйва',
-            description: '',
-            ogDescription: '',
-        },
-    }
+    // newsSEO: {
+    //     uz: {
+    //         title: "Sirius-tech kompaniyasining so'nggi yangiliklari, aksiyalar va mobil aksessuarlardagi tendentsiyalaridan xabardor bo'ling",
+    //         description: "Mobil aksessuarlar tendentsiyalari haqidagi eng yangi yangilanishlar, eksklyuziv yangiliklar uchun bizning blogimizga kiring va qiziqarli sotuvlarimizni o'tkazib yubormang. Sirius-tech’da arzon texnologik yechimlar va uslub ilhomini kashf eting.",
+    //         ogDescription: "Mobil aksessuarlar tendentsiyalari haqidagi eng yangi yangilanishlar, eksklyuziv yangiliklar uchun bizning blogimizga kiring va qiziqarli sotuvlarimizni o'tkazib yubormang. Sirius-tech’da arzon texnologik yechimlar va uslub ilhomini kashf eting.",
+    //     },
+    //     ru: {
+    //         title: 'Будьте в курсе последних новостей и тенденций Sirius-tech в области мобильных аксессуаров.',
+    //         description: 'Загляните в наш блог, чтобы узнать самые свежие новости о тенденциях в области мобильных аксессуаров, эксклюзивные новости и не пропустите наши захватывающие распродажи. Откройте для себя доступные технологические решения и вдохновение для стиля в Sirius-tech.',
+    //         ogDescription: 'Загляните в наш блог, чтобы узнать самые свежие новости о тенденциях в области мобильных аксессуаров, эксклюзивные новости и не пропустите наши захватывающие распродажи. Откройте для себя доступные технологические решения и вдохновение для стиля в Sirius-tech.',
+    //     },
+    // },
 };
 
 export const {index , catalogSEO ,driveSEO , contactSEO ,  newsSEO, aboutUsSEO ,creditSEO , insuranceSEO ,serviceSEO} = seoContent
